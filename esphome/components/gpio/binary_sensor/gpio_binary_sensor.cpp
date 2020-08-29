99c4333feb32c7614059f34d82488aee1cc42571
Jerzy Kołosowski <jerzy@kolosowscy.pl>
2020-08-30 01:16:47 +0200

    add support for frequency input on gpio binary sensor

--- esphome/components/gpio/binary_sensor/gpio_binary_sensor.cpp
+++ esphome/components/gpio/binary_sensor/gpio_binary_sensor.cpp
@@ -8,7 +8,7 @@ static const char *TAG = "gpio.binary_sensor";
 
 void GPIOBinarySensor::setup() {
   this->pin_->setup();
-  this->publish_initial_state(this->pin_->digital_read());
+  this->publish_initial_state(this->read_state());
 }
 
 void GPIOBinarySensor::dump_config() {
@@ -16,9 +16,25 @@ void GPIOBinarySensor::dump_config() {
   LOG_PIN("  Pin: ", this->pin_);
 }
 
-void GPIOBinarySensor::loop() { this->publish_state(this->pin_->digital_read()); }
+void GPIOBinarySensor::loop() { this->publish_state(this->read_state()); }
 
 float GPIOBinarySensor::get_setup_priority() const { return setup_priority::HARDWARE; }
+
+bool GPIOBinarySensor::read_state() {
+  bool pin_state = this->pin_->digital_read();
+    
+  if (this->frequency_ > 0.0f) {
+    int wait_time = 1000.0f / this->frequency_ * 0.6f;
+    
+    unsigned long start = millis();
+    while (millis() - start < wait_time && pin_state) {
+      delay(1);
+      pin_state = pin_->digital_read();
+    }
+  }
+
+  return pin_state;
+}
 
 }  // namespace gpio
 }  // namespace esphome
